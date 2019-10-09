a62e440b3d68aecd563d1aba5252b92f819dfe9a
Nadnus <fernando.delosheros@utec.edu.pe>
2019-10-09 12:46:20 -0500

    setting repo2

--- Observer.java
+++ Observer.java
@@ -1,4 +1,12 @@
 class Observer {
+    WeatherData myReference;
+    Float myTemp;
+    Float myHumidity;
+    Float myPressure;
+
     public void update(){
+        myTemp =myReference.temp;
+        myHumidity = myReference.humidity;
+        myPressure =myReference.pressure;
         
     }
